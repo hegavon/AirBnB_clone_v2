0675100fcc5fcb4429f4c45a319d1e120da5bcbf
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-03-19 18:27:58 +0100

    Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,80 +1,58 @@
 #!/usr/bin/python3
-"""This is the file storage class for AirBnB"""
+"""Module for FileStorage class."""
 import json
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
 from models.place import Place
+from models.amenity import Amenity
 from models.review import Review
-import shlex
 
 
 class FileStorage:
-    """This class serializes instances to a JSON file and
-    deserializes JSON file to instances
-    Attributes:
-        __file_path: path to the JSON file
-        __objects: objects will be stored
+    """Class for serializing and deserializing
+    instances to JSON file and vice-versa.
     """
+
     __file_path = "file.json"
     __objects = {}
+    class_dict = {"BaseModel": BaseModel, "User": User, "Place": Place,
+                  "Amenity": Amenity, "City": City, "Review": Review,
+                  "State": State}
 
-    def all(self, cls=None):
-        """returns a dictionary
-        Return:
-            returns a dictionary of __object
-        """
-        dic = {}
-        if cls:
-            dictionary = self.__objects
-            for key in dictionary:
-                partition = key.replace('.', ' ')
-                partition = shlex.split(partition)
-                if (partition[0] == cls.__name__):
-                    dic[key] = self.__objects[key]
-            return (dic)
-        else:
-            return self.__objects
+    def all(self):
+        """Returns the dictionary __objects."""
+        return FileStorage.__objects
 
-    def new(self, obj):
-        """sets __object to given obj
-        Args:
-            obj: given object
-        """
-        if obj:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            self.__objects[key] = obj
+    @classmethod
+    def new(cls, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
-    def save(self):
-        """serialize the file path to JSON file path
-        """
-        my_dict = {}
-        for key, value in self.__objects.items():
-            my_dict[key] = value.to_dict()
-        with open(self.__file_path, 'w', encoding="UTF-8") as f:
-            json.dump(my_dict, f)
+    @classmethod
+    def save(cls):
+        """Serializes __objects to the JSON file (path: __file_path)."""
+        serialized_objects = {}
+        for key, value in FileStorage.__objects.items():
+            serialized_objects[key] = value.to_dict()
+        with open(cls.__file_path, "w", encoding="utf-8") as json_file:
+            json.dump(serialized_objects, json_file)
 
-    def reload(self):
-        """serialize the file path to JSON file path
-        """
+    @classmethod
+    def reload(cls):
+        """Deserializes the JSON file to __objects."""
         try:
-            with open(self.__file_path, 'r', encoding="UTF-8") as f:
-                for key, value in (json.load(f)).items():
-                    value = eval(value["__class__"])(**value)
-                    self.__objects[key] = value
+            with open(cls.__file_path, "r", encoding="utf-8") as json_file:
+                loaded_objects = json.load(json_file)
+            for key, value in loaded_objects.items():
+                class_name = value['__class__']
+                if class_name in cls.class_dict:
+                    obj_class = cls.class_dict[class_name]
+                    obj = obj_class(**value)
+                    FileStorage.__objects[key] = obj
+                else:
+                    print(f"Class {class_name} not found in class_dict")
         except FileNotFoundError:
             pass
-
-    def delete(self, obj=None):
-        """ delete an existing element
-        """
-        if obj:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            del self.__objects[key]
-
-    def close(self):
-        """ calls reload()
-        """
-        self.reload()
