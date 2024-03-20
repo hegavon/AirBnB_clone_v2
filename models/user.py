077ddab59ea65fa105d51463e85caf14afad99ca
Victor Amajuoyi <victorgavon@gmail.com>
2024-03-20 15:38:22 +0100

    AirBnB CloneV2 - user.py update

--- models/user.py
+++ models/user.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""This is model defines a class user"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from models.stringtemplates import HBNB_TYPE_STORAGE, DB
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from os import getenv
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    if getenv(HBNB_TYPE_STORAGE) == DB:
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
