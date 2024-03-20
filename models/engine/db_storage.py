d62400da3fdc549fd4a0cbcc189c2593e937c800
Victor Amajuoyi <victorgavon@gmail.com>
2024-03-21 00:38:48 +0100

    AirBnB CloneV2 - db_storage.py update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,62 +1,26 @@
 #!/usr/bin/python3
 """Defines the DBStorage engine."""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from os import getenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from models.base_model import Base
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
-class DBStorage:
-    """Represents a database storage engine."""
+class Review(BaseModel, Base):
+    """Represents a review in the system."""
 
-    def __init__(self):
-        """Initialize a new DBStorage instance."""
-        db_user = getenv("HBNB_MYSQL_USER")
-        db_pwd = getenv("HBNB_MYSQL_PWD")
-        db_host = getenv("HBNB_MYSQL_HOST")
-        db_name = getenv("HBNB_MYSQL_DB")
-        self.__engine = create_engine(f"mysql+mysqldb://{db_user}:{db_pwd}@{db_host}/{db_name}", pool_pre_ping=True)
-        if getenv("HBNB_ENV") == "test":
-            Base.metadata.drop_all(self.__engine)
+    __tablename__ = "reviews"
 
-    def all(self, cls=None):
-        """Query on the current database session all objects of the given class."""
-        session = self.__session
-        query_classes = [State, City, User, Place, Review, Amenity]
-        if cls:
-            if isinstance(cls, str):
-                cls = eval(cls)
-            query_classes = [cls]
-        objs = []
-        for query_class in query_classes:
-            objs.extend(session.query(query_class).all())
-        return {f"{type(o).__name__}.{o.id}": o for o in objs}
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    else:
+        place_id, user_id, text = "", "", ""
 
-    def new(self, obj):
-        """Add obj to the current database session."""
-        self.__session.add(obj)
+    def __init__(self, *args, **kwargs):
+        """Initializes a review."""
+        super().__init__(*args, **kwargs)
 
-    def save(self):
-        """Commit all changes to the current database session."""
-        self.__session.commit()
-
-    def delete(self, obj=None):
-        """Delete obj from the current database session."""
-        if obj:
-            self.__session.delete(obj)
-
-    def reload(self):
-        """Create all tables in the database and initialize a new session."""
-        Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
-
-    def close(self):
-        """Close the working SQLAlchemy session."""
-        self.__session.close()
+    def __repr__(self):
+        """Returns the string representation of the review."""
+        return f"<Review {self.id}>"
