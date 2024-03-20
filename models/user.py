4be26d77e025e05c96adc0912159f66d26b591a9
Victor Amajuoyi <victorgavon@gmail.com>
2024-03-21 00:06:55 +0100

    AirBnB CloneV2 - user.py update

--- models/user.py
+++ models/user.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
 """This is model defines a class user"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
-from os import getenv
+from models.base_model import BaseModel, Base
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
+    """Represents a user in the system."""
+
     __tablename__ = "users"
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+
+    places = relationship("Place", cascade="all, delete-orphan", backref="user")
+    reviews = relationship("Review", cascade="all, delete-orphan", backref="user")
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a user."""
+        super().__init__(*args, **kwargs)
+
+    def __repr__(self):
+        """Returns the string representation of the user."""
+        return f"<User {self.id}>"
