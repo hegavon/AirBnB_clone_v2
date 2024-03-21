a9d4e46ff513276f3f0de7f041e4938738c11a65
Victor Amajuoyi <victorgavon@gmail.com>
2024-03-21 01:35:30 +0100

    AirBnB CloneV2 - pycodestyle debugging

--- models/review.py
+++ models/review.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+from models.stringtemplates import HBNB_TYPE_STORAGE, DB
+from sqlalchemy import String, Column, ForeignKey
 from os import getenv
 
 
 class Review(BaseModel, Base):
-    """Represents a review in the system."""
-
-    __tablename__ = "reviews"
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+    """ Review classto store review information """
+    __tablename__ = 'reviews'
+    if getenv(HBNB_TYPE_STORAGE) == DB:
         text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
     else:
         place_id = ""
         user_id = ""
         text = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initializes a review."""
-        super().__init__(*args, **kwargs)
-
-    def __repr__(self):
-        """Returns the string representation of the review."""
-        return f"<Review {self.id}>"
