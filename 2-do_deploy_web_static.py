c325b0799ae680fc16951e3cde38e0879ab98759
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-04-04 00:19:13 +0100

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,30 @@
 #!/usr/bin/python3
 """
-Fabric script to distribute an archive to web servers
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
 
-from fabric.api import *
+from fabric.api import put, run, env
 from os.path import exists
-from fabric.exceptions import NetworkError
-
 env.hosts = ['54.160.73.250', '52.90.22.193']
-env.user = 'ubuntu'
-env.key_filename = ['~/.ssh/id_rsa']
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to web servers
-    """
-    if not exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
-    filename = archive_path.split('/')[-1]
-    foldername = filename.split('.')[0]
-
     try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p /data/web_static/releases/{}/'.format(foldername))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(filename, foldername))
-        run('rm /tmp/{}'.format(filename))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'
-            .format(foldername, foldername))
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(foldername))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current'.format(foldername))
-        print("New version deployed!")
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except NetworkError as e:
-        print("Error: Network issue occurred - {}".format(e))
-        return False
-    except Exception as e:
-        print("Error: An unexpected error occurred - {}".format(e))
+    except:
         return False
