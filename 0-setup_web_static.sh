a5efa13cc29d04553b1015dc604df83e1e37e3ca
Victor Amajuoyi <victorgavon@gmail.com>
2024-04-05 00:23:27 +0100

    AirBnB Deploy Static task 0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,14 +15,8 @@ echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu:ubuntu /data
-sudo chgrp -R ubuntu:ubuntu /data
+sudo chown -R ubuntu /data
+sudo chgrp -R ubuntu /data
 
 # Update Nginx configuration
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-
-# Restart Nginx
-
-sudo service nginx restart
-
-exit 0
