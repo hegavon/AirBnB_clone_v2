a3b2df1c1deef2f024df2545fa24f8fa51479816
Victor Amajuoyi <victorgavon@gmail.com>
2024-04-04 16:16:16 -0700

    AirBnB Deploy Static task 0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,11 +12,17 @@ echo "<html>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -R ubuntu /data/
-sudo chgrp -R ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data
+sudo chgrp -R ubuntu:ubuntu /data
 
 # Update Nginx configuration
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+# Restart Nginx
+
+sudo service nginx restart
+
+exit 0
